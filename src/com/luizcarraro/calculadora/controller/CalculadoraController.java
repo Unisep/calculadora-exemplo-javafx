643e7d272f261e2d6f6b81e53c170e8ea2c435d8
Luiz Carraro <lacarraro@gmail.com>
2016-04-07 16:18:09 -0300

    Calculator java and fxml files

---
+++ src/com/luizcarraro/calculadora/controller/CalculadoraController.java
@@ -0,0 +1,58 @@
+package com.luizcarraro.calculadora.controller;
+
+import com.luizcarraro.calculadora.model.Calculation;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class CalculadoraController {
+
+    @FXML
+    private Text output;
+    
+    @FXML
+    private Label logoImage;
+    
+    private long number1 = 0;
+    private String operator = "";
+    private boolean start = true;
+
+    private Calculation model = new Calculation();
+
+    @FXML
+    private void processNumpad(ActionEvent event) {
+        if (start) {
+            output.setText("");
+            start = false;
+        }
+
+        String value = ((Button)event.getSource()).getText();
+        output.setText(output.getText() + value);
+    }
+
+    @FXML
+    private void processOperator(ActionEvent event) {
+        String value = ((Button)event.getSource()).getText();
+
+        if (!"=".equals(value)) {
+            if (!operator.isEmpty())
+                return;
+
+            operator = value;
+            number1 = Long.parseLong(output.getText());
+            output.setText("");
+        }
+        else {
+            if (operator.isEmpty())
+                return;
+
+            output.setText(String.valueOf(model.calculate(number1, Long.parseLong(output.getText()), operator)));
+            operator = "";
+            start = true;
+        }
+    }
+}
